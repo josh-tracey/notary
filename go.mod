99f7de7361f36e45cc521e30af0d19c8c37eafcb
Joshua Tracey <joshua.tracey@gmail.com>
2022-09-06 08:17:55 +1000

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/josh-tracey/notary
+
+go 1.19
+
+require github.com/golang-jwt/jwt/v4 v4.4.2
---
+++ notary.go
@@ -0,0 +1,30 @@
+package notary
+
+import "github.com/golang-jwt/jwt/v4"
+
+type Notary struct {
+	secret string
+}
+
+func NewNotary(secret string) *Notary {
+	return &Notary{secret: secret}
+}
+
+func (n *Notary) VerifyToken(token string) (bool, error) {
+	_, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return []byte(n.secret), nil
+	})
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func (n *Notary) SignToken(secret string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	tokenString, err := token.SignedString([]byte(n.secret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
